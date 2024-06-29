70163876161618670b08cf7787c1d7a285770197
adimerpaul <adimer101@gmail.com>
2024-06-29 06:00:24 -0400

    actulizadno para forn de acmbio de proximo y activo

--- front/src/pages/movies/MoviesPage.vue
+++ front/src/pages/movies/MoviesPage.vue
@@ -2,7 +2,7 @@
 <q-page class="q-pa-xs bg-grey-3">
   <div class="row">
     <div class="col-12">
-      <q-table dense :loading="loading" :rows="movies">
+      <q-table dense :loading="loading" :rows="movies" :filter="filter" :rows-per-page-options="[0]" :columns="columns">
         <template v-slot:top-right>
           <q-btn label="Crear" no-caps size="12px" color="green" icon="add_circle_outline" class="text-bold" @click="dialogMovieClick"/>
           <q-input v-model="filter" outlined dense placeholder="Buscar">
@@ -11,12 +11,35 @@
             </template>
           </q-input>
         </template>
+        <template v-slot:body-cell-proximo="props">
+          <q-td :props="props">
+            <q-btn dense :color="props.row.proximo ? 'green' : 'red'" text-color="white" @click="clickProximo(props.row)" no-caps :loading="loading">
+              {{props.row.proximo ? 'Si' : 'No'}}
+            </q-btn>
+          </q-td>
+        </template>
+        <template v-slot:body-cell-activo="props">
+          <q-td :props="props">
+            <q-btn dense :color="props.row.activo ? 'green' : 'red'" text-color="white" @click="clickActivo(props.row)" no-caps :loading="loading">
+              {{props.row.activo ? 'Si' : 'No'}}
+            </q-btn>
+          </q-td>
+        </template>
       </q-table>
     </div>
   </div>
   <q-dialog v-model="dialogMovie">
     <q-card style="width: 700px; max-width: 90vw;">
+      <q-card-section class="q-pb-none">
+          <div class="row items-center">
+            <div class="col text-bold">{{movie.id ? 'Editar' : 'Crear'}} Película</div>
+            <div class="col-auto">
+              <q-btn dense icon="close" flat @click="dialogMovie = false" :loading="loading"/>
+            </div>
+          </div>
+      </q-card-section>
       <q-card-section>
+        <q-form @submit="movieSubmit">
         <label class="text-subtitle2">Película</label>
         <q-input v-model="movie.title" outlined dense label="Título" placeholder="Título">
           <template v-slot:after>
@@ -26,10 +49,12 @@
         <label class="text-subtitle2">Datos</label>
         <q-input v-model="movie.json" outlined dense label="JSON" placeholder="JSON" type="textarea"/>
         <q-checkbox v-model="movie.proximo" label="Próximo"/>
+        <q-checkbox v-model="movie.activo" label="Activo"/>
         <q-card-actions align="right">
           <q-btn label="Cancelar" color="negative" @click="dialogMovie = false" :loading="loading"/>
-          <q-btn label="Guardar" color="green" @click="dialogMovie = false" :loading="loading"/>
+          <q-btn label="Guardar" color="green" type="submit"/>
         </q-card-actions>
+        </q-form>
         <q-list bordered dense separator>
           <q-item v-for="item in resultMovies" :key="item.id">
             <q-item-section avatar>
@@ -63,10 +88,18 @@ export  default {
   data() {
     return {
       movies: [],
+      columns: [
+        {name: 'actions', label: 'Acciones', align: 'center', field: 'actions'},
+        {name: 'id', label: 'ID', align: 'center', field: 'id'},
+        {name: 'title', label: 'Título', align: 'left', field: 'title'},
+        {name: 'proximo', label: 'Próximo', align: 'center', field: 'proximo'},
+        {name: 'activo', label: 'Activo', align: 'center', field: 'activo'},
+      ],
       movie: {
         title: '',
         json: '',
         proximo: false,
+        activo: true,
       },
       urlSearch:"https://api.themoviedb.org/3/search/movie?api_key=eccf4da78932269df065470af1b8c6d9&language=es-ES&query=",
       urlMovie:"https://api.themoviedb.org/3/movie/xxxxx?api_key=eccf4da78932269df065470af1b8c6d9&language=es-ES",
@@ -77,8 +110,39 @@ export  default {
     }
   },
   methods: {
+    clickActivo(movie) {
+      movie.activo = !movie.activo
+      this.movie = movie
+      this.movieSubmit()
+    },
+    clickProximo(movie) {
+      movie.proximo = !movie.proximo
+      this.movie = movie
+      this.movieSubmit()
+    },
+    movieSubmit() {
+      this.loading = true
+      if (this.movie.id) {
+        this.$axios.put('/movies/' + this.movie.id, this.movie)
+          .then(() => {
+            this.getMovies()
+            this.dialogMovie = false
+          }).finally(() => {
+          this.loading = false
+          })
+      } else {
+        this.$axios.post('/movies', this.movie)
+          .then(() => {
+            this.getMovies()
+            this.dialogMovie = false
+          }).finally(() => {
+          this.loading = false
+          })
+      }
+    },
     copiMovie(item) {
-      console.log(item)
+      // console.log(item)
+      this.movie.title = item.title
       const url = this.urlMovie.replace('xxxxx', item.id)
       this.$axios.get(url)
         .then(res => {
@@ -104,6 +168,7 @@ export  default {
         title: '',
         json: '',
         proximo: false,
+        activo: true,
       }
     },
     async getMovies() {
